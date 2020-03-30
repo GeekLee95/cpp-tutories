5a7afa875e6cc6abe39dc18915810e2666b14ab5
GeekLee95 <920033291@qq.com>
2020-03-31 00:39:43 +0800

    update easy cuda example

--- examples/cmake/5_usecuda/main.cu
+++ examples/cmake/5_usecuda/main.cu
@@ -1,6 +1,20 @@
 
 #include <stdio.h>
+#include <cuda_runtime.h>
+
+__global__ void add (int a, int b, int *c) {
+    *c = a + b;
+    printf("into add");
+}
 int main(int argv, char** argc ) {
     printf( "Hello, World!\n" );
+    int c;
+    int *device_c;
+    cudaMalloc((void**)&device_c, sizeof(int));
+    add<<<1, 1>>>(2, 7, device_c);
+    cudaMemcpy(&c, device_c, sizeof(int),
+               cudaMemcpyDeviceToHost);
+    printf("2 + 7 = %d\n", c);
+    cudaFree(device_c); 
     return 0;
 }
